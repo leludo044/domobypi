bf35111bad75cf0cba5838d750f18272771f8e9c
leludo044 <leludo044@gmail.com>
2015-08-11 23:54:43 +0200

    Comments.

--- app/net/leludo/domobypi/model/led/GpioConnected.java
+++ app/net/leludo/domobypi/model/led/GpioConnected.java
@@ -2,7 +2,14 @@ package net.leludo.domobypi.model.led;
 
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 
+/**
+ * Interface for all component that need to be connected to the GPIO. This is
+ * used by ComponentFactory to connect a newly created real led
+ * 
+ * @see ComponentFactory
+ *
+ */
 public interface GpioConnected {
 
-	void connect(final GpioPinDigitalOutput pin) ;
+	void connect(final GpioPinDigitalOutput pin);
 }
