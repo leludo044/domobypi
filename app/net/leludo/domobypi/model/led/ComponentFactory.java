156b570b88358c037cf2da3a0bc1eef6768b528d
leludo044 <leludo044@gmail.com>
2015-08-11 23:50:15 +0200

    Refactoring and comments.

--- app/net/leludo/domobypi/model/led/ComponentFactory.java
+++ app/net/leludo/domobypi/model/led/ComponentFactory.java
@@ -9,15 +9,28 @@ import com.pi4j.io.gpio.PinState;
 import net.leludo.pi.hardware.PiPins;
 import play.Logger;
 
+/**
+ * Factory for real led connected to hardware
+ */
 public class ComponentFactory {
 
+	/** Factory instance */
 	static ComponentFactory instance;
+	
+	/** GPIO Controller to use */
 	final GpioController gpio;
 
+	/**
+	 * Constructor. The factory can not be instanciated. It's a singleton
+	 */
 	private ComponentFactory() {
 		gpio = GpioFactory.getInstance();
 	}
 
+	/**
+	 * 
+	 * @return The unique factory instance
+	 */
 	public static final ComponentFactory getInstance() {
 		if (instance == null) {
 			instance = new ComponentFactory();
@@ -25,6 +38,13 @@ public class ComponentFactory {
 		return instance;
 	}
 
+	/**
+	 * Create a real led
+	 * @param type Led type to create
+	 * @param id Led ID 
+	 * @param pin Pin number the led is connected on. Pin number is the sequential from 1 to 24
+	 * @return The real led
+	 */
 	public Led createComponent(final String type, final String id,
 			final PiPins pin) {
 		Led component = null;
@@ -36,18 +56,15 @@ public class ComponentFactory {
 		return component;
 	}
 
+	/**
+	 * Connect a led to a GPIO related to the pin number
+	 * @param component The led 
+	 * @param name The name associated to the GPIO
+	 * @param pinNumber The pin number
+	 */
 	private void connect(GpioConnected component, String name, PiPins pinNumber) {
 		GpioPinDigitalOutput pin = this.gpio.provisionDigitalOutputPin(pinNumber.getPin(), name, PinState.LOW);
 		pin.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
 		component.connect(pin);
-	}
-	
-	
-	public GpioController getGpioController() {
-		return gpio;
-	}
-
-	public void close() {
-		//gpio.shutdown();
-	}
+	}	
 }
