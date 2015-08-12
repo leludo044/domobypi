3754061e8b1d8fc615ed22c2401d7e7e6d8f6e95
Ludovic <leludo044@gmail.com>
2015-08-12 16:55:17 +0200

    Integration tests.

--- app/Global.java
+++ app/Global.java
@@ -7,9 +7,11 @@ import play.Logger;
 public class Global extends GlobalSettings {
 
 	/** Config file name */
-	private static final String CONFIG_DEV_FILENAME = "config_dev.json";
-	private static final String CONFIG_PROD_FILENAME = "config_prod.json";
+	private static final String CONFIG_PREFIX_FILENAME = "config_";
+	private static final String CONFIG_EXTENSION_FILENAME = ".json";
 
+	String runningMode ; 
+	
 	/**
 	 * On application startup
 	 * 
@@ -19,8 +21,9 @@ public class Global extends GlobalSettings {
 	@Override
 	public void onStart(Application app) {
 
-		Logger.info("Starting application (" + (app.isDev() ? "mode DEV" : "Mode PROD") + ")...");
-		String configFilename = app.isDev() ? CONFIG_DEV_FILENAME : CONFIG_PROD_FILENAME;
+		this.detectRunningMode(app);
+		Logger.info("Starting application (mode "+ this.runningMode + ")...");
+		String configFilename = CONFIG_PREFIX_FILENAME+this.runningMode+CONFIG_EXTENSION_FILENAME;
 		Logger.info("Config file " + configFilename + " expected.");
 		ApplicationContext ac = ApplicationContext.getInstance();
 		try {
@@ -34,6 +37,18 @@ public class Global extends GlobalSettings {
 	@Override
 	public void onStop(Application arg0) {
 		Logger.info("Application has stopped.");
+	}
+	
+	private void detectRunningMode(Application app) {
+		if (app.isDev()) {
+			this.runningMode = "dev" ;
+		} else if (app.isProd()) {
+			this.runningMode = "prod" ;			
+		} else if (app.isTest()) {
+			this.runningMode = "test" ;			
+		} else {
+			this.runningMode = "unknows" ;			
+		}
 	}
 
 }
--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,29 +1,78 @@
 import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.HTMLUNIT;
 import static play.test.Helpers.fakeApplication;
-import static play.test.Helpers.inMemoryDatabase;
 import static play.test.Helpers.running;
 import static play.test.Helpers.testServer;
+
+import java.util.List;
+
+import net.leludo.domobypi.bootstrap.ApplicationContext;
+import net.leludo.domobypi.model.sensor.AbstractSensor;
 
 import org.junit.Test;
 
 import play.libs.F.Callback;
+import play.libs.ws.WS;
+import play.libs.ws.WSResponse;
 import play.test.TestBrowser;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class IntegrationTest {
 
-    /**
-     * add your integration test here
-     * in this example we just check if the welcome page is being shown
-     */
-    @Test
-    public void test() {
-        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
-            public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333");
-                assertThat(browser.pageSource()).contains("Your new application is ready.");
-            }
-        });
-    }
+	/**
+	 * add your integration test here in this example we just check if the
+	 * welcome page is being shown
+	 */
+	@Test
+	public void test() {
+		running(testServer(3333, fakeApplication(new Global())), HTMLUNIT,
+				new Callback<TestBrowser>() {
+					public void invoke(TestBrowser browser) {
+						browser.goTo("http://localhost:3333");
+						System.out.println("toto"+browser.pageSource());
+						assertThat(browser.pageSource()).contains("domobyPi");
+						// assertThat(browser.$("#title").getText()).isEqualTo("domobyPi");
+					}
+				});
+	}
+
+	@Test
+	public void testModule() {
+		running(fakeApplication(new Global()), new Runnable() {
+
+			ApplicationContext context = ApplicationContext.getInstance();
+
+			@Override
+			public void run() {
+				assertThat(context.getSensors()).isNotNull();
+				assertThat(context.getSensors().size()).isEqualTo(2);
+				assertThat(context.getLeds()).isNotNull();
+				assertThat(context.getLeds().size()).isEqualTo(2);
+			}
+
+		});
+	}
+
+	@Test
+	public void testWsSensors() {
+		running(testServer(3333, fakeApplication(new Global())),
+				new Runnable() {
+
+					@Override
+					public void run() {
+						WSResponse response = WS
+								.url("http://localhost:3333/json/sensors")
+								.get().get(1000);
+						String jsonResponse = response.asJson().toString() ;
+						System.out.println(jsonResponse);
+						assertThat(response.getStatus()).isEqualTo(200);
+						assertThat(jsonResponse)
+								.isEqualTo(
+										"[{\"id\":\"random01\",\"type\":\"random\",\"frequency\":5000,\"model\":\"inconnu\",\"min\":0,\"max\":24,\"led\":\"red\"},{\"id\":\"random02\",\"type\":\"random\",\"frequency\":10000,\"model\":\"inconnu\",\"min\":0,\"max\":30,\"led\":null}]");
+					}
+
+				});
+	}
 
 }
